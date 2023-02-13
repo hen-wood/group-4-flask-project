6ab8cdc770146d3b391288e2ea84271273d887fb
enkhzayaqt <jini.zaya@gmail.com>
2023-02-12 21:43:05 -0800

    API routes done

--- app/api/comments_routes.py
+++ app/api/comments_routes.py
@@ -1,53 +1,52 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required
-from app.models.channel_comment import ChannelComment
+from app.models import ChannelComment, db
 
 
 comments_routes = Blueprint('comments', __name__)
 
 
-@comments_routes.route('/:channelId')
-@login_required
+@comments_routes.route('/')
+# @login_required
 def get_comments():
     """
     Query for all comments and returns them in a date order
     """
-    results = ChannelComment.query.all()
-    return results
+    comments = ChannelComment.query.all()
+    return {'comments': [comment.to_dict() for comment in comments]}
+
 
 
 @comments_routes.route('/', methods=['POST'])
-@login_required
 def add_comment():
     """
     Add comment and returns it in a date order
     """
     request_data = request.get_json()
+
     comment = ChannelComment(
-            user_id=request_data.user_id,
-            channel_id=request_data.channel_id,
-            content=request_data.content
+            user_id=request_data['user_id'],
+            channel_id=request_data['channel_id'],
+            content=request_data['content']
         )
     db.session.add(comment)
     db.session.commit()
-    return comment
+    return comment.to_dict()
 
-
-@comments_routes.route('/<id>', methods=['PUT'])
-@login_required
+@comments_routes.route('/<int:id>', methods=['PUT'])
 def edit_comment(id):
     """
     Edit comment and return
     """
     comment = ChannelComment.query.get(id)
-    content = request.json["content"]
+    content = request.json['content']
     comment.content = content
     db.session.commit()
-    return comment
+    return comment.to_dict()
 
 
-@comments_routes.route('/<id>', methods=['DELETE'])
-@login_required
+@comments_routes.route('/<int:id>', methods=['DELETE'])
+# @login_required
 def delete_comment(id):
     """
     Delete comment
