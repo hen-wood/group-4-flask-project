fd9ebc3fb9edbf690665fe62162ab0272f445445
Henry W <henry.woodmansee@gmail.com>
2023-02-15 18:45:14 -0500

    added additional domains to allowed cors domain list to include http

--- app/api/sockets.py
+++ app/api/sockets.py
@@ -9,7 +9,10 @@ if os.environ.get("FLASK_ENV") == "production":
     origins = [
         "https://group-4-discord.onrender.com/",
         "https://discord-clone-maks.onrender.com/",
-        "https://discordance.onrender.com/"
+        "https://discordance.onrender.com/",
+        "http://group-4-discord.onrender.com/",
+        "http://discord-clone-maks.onrender.com/",
+        "http://discordance.onrender.com/"
     ]
 else:
     origins = "*"
